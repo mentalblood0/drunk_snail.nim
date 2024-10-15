3479b230585bc4dbc611afc168fcea3d20465175
Нечепоренко Степан <necheporenko_s_iu@goznak.ru>
2024-10-15 14:27:33 +0300

    massive refactoring, full subtemplates support

--- drunk_snail.nim
+++ drunk_snail.nim
@@ -6,20 +6,52 @@ import regex
 const open = "<!--"
 const close = "-->"
 
-const expression_regex = re2(
-  "(?P<open>" & open &
-    r") *(?:\((?P<optional>optional)\))?\((?P<operator>[A-Za-z]+)\)(?P<name>[A-Za-z0-9]+) *(?P<close>" &
-    close & r")"
-)
+func expression_regex(operator: static string): auto =
+  return re2(
+    "(?P<open>" & open &
+      r") *(?:\((?P<optional>optional)\))?\((?P<operator>" & operator &
+    r")\)(?P<name>[A-Za-z0-9]+) *(?P<close>" &
+      close & r")"
+  )
 
-type Expression =
-  tuple[boundaries: Slice[system.int], optional: bool, operator: string, name: string]
+const param_regex = expression_regex("param")
+const ref_regex = expression_regex("ref")
 
-type Line = tuple[source: string, expressions: seq[Expression]]
-type External = tuple[left: string, right: string]
-type Template* = tuple[lines: seq[Line]]
-type Templates* = Table[string, Template]
 type
+  Expression = tuple[name: string, optional: bool]
+  Bounds = tuple[left: string, right: string]
+
+  ParamLineTokenKind = enum
+    pltPlain,
+    pltParam
+
+  ParamLineToken = ref ParamLineTokenObj
+  ParamLineTokenObj = object
+    case kind: ParamLineTokenKind
+    of pltPlain:
+      value: string
+    of pltParam:
+      expression: Expression
+
+  LineKind = enum
+    lPlain,
+    lParams,
+    lRef
+
+  Line = ref LineObj
+  LineObj = object
+    case kind: LineKind
+    of lPlain:
+      value: string
+    of lParams:
+      tokens: seq[ParamLineToken]
+    of lRef:
+      expression: Expression
+      bounds: Bounds
+
+  Template* = tuple[lines: seq[Line]]
+  Templates* = Table[string, Template]
+
   Params* = Table[string, Value]
   ValueKind = enum
     vkValuesList
@@ -30,6 +62,18 @@ type
     case kind: ValueKind
     of vkValuesList: values_list: seq[string]
     of vkParamsList: params_list: seq[Params]
+
+  ParseError* = object of ValueError
+  RenderError* = object of ValueError
+
+func new_expression(line: string, m: regex.RegexMatch2): Expression =
+  return (name: line[m.group("name")], optional: len(line[m.group(
+      "optional")]) > 0)
+
+func new_bounds(line: string, m: regex.RegexMatch2): Bounds = (line[0 ..<
+    m.boundaries.a], line[m.boundaries.b + 1 .. ^1])
+
+func `&`(a: Bounds, b: Bounds): Bounds = (b.left & a.left, a.right & b.right)
 
 func values_list(l: seq[string]): Value =
   Value(kind: vkValuesList, values_list: l)
@@ -44,78 +88,105 @@ func len(v: Value): int =
     return len(v.params_list)
 
 func new_line(line: string): Line =
-  for m in find_all(line, expression_regex):
-    result.expressions.add(
-      (
-        boundaries: m.boundaries,
-        optional: len(line[m.group("optional")]) > 0,
-        operator: line[m.group("operator")],
-        name: line[m.group("name")],
-      )
-    )
-  result.source = line
+  let refs = find_all(line, ref_regex)
+  let params = find_all(line, param_regex)
+  if len(refs) > 1:
+    raise new_exception(ParseError, "Line `" & line & "` contain more then one reference expressions")
+  if len(params) > 0 and len(refs) > 0:
+    raise new_exception(ParseError, "Line `" & line & "` mixes parameters and references expressions")
 
-func rendered*(
+  if len(refs) > 0:
+    let r = refs[0]
+    return Line(kind: lRef, expression: new_expression(line, r),
+        bounds: new_bounds(line, r))
+  elif len(params) > 0:
+    result = Line(kind: lParams)
+    var b = 0
+    for p in params:
+      let plain = line[b ..< p.boundaries.a]
+      if len(plain) > 0:
+        result.tokens.add ParamLineToken(kind: pltPlain, value: plain)
+      b = p.boundaries.b + 1
+      result.tokens.add ParamLineToken(kind: pltParam,
+          expression: new_expression(line, p))
+    let plain = line[b .. ^1]
+    if len(plain) > 0:
+      result.tokens.add ParamLineToken(kind: pltPlain, value: line[b .. ^1])
+  else:
+    return Line(kind: lPlain, value: line)
+
+proc rendered*(
   t: Template,
   params: Params = Params(init_table[string, Value]()),
   templates: Templates = Templates(init_table[string, Template]()),
+  bounds: Bounds = ("", "")
 ): string
 
-func rendered(
-    line: Line, params: Params, templates: Templates, external: External = ("", "")
+proc rendered(
+    line: Line, params: Params, templates: Templates, external: Bounds
 ): string =
-  let min_len = block:
-    var r = 1
-    for i, e in line.expressions:
-      if e.optional or not (e.name in params):
-        continue
-      if e.operator == "template":
-        r = 0
-        break
-      let n = len(params[e.name])
-      if r == 1 or n < r:
-        r = n
-    r
-  for i in 0 ..< min_len:
-    if i != 0:
-      result &= '\n'
-    result &= external.left
-    var b = 0
-    for e in line.expressions:
-      if e.boundaries.a > 0:
-        result &= line.source[b ..< e.boundaries.a]
-      if e.operator == "param":
-        if not (
-          e.optional and (
-            (not (e.name in params)) or (len(params[e.name]) == 0) or
-            (params[e.name].kind != vkValuesList)
-          )
-        ):
-          result &= params[e.name].values_list[i]
-      elif e.operator == "ref":
+
+  if line.kind == lPlain:
+    return line.value
+
+  elif line.kind == lParams:
+    let min_len = block:
+      var r = 1
+      for t in line.tokens:
+        if t.kind == pltParam:
+          let e = t.expression
+          if e.optional or not (e.name in params):
+            continue
+          let n = len(params[e.name])
+          if r == 1 or n < r:
+            r = n
+      r
+    for i in 0 ..< min_len:
+      if i != 0:
+        result &= '\n'
+      for t in line.tokens:
+        if t.kind == pltPlain:
+          result &= t.value
+        elif t.kind == pltParam:
+          let e = t.expression
+          if not (
+            e.optional and (
+              (not (e.name in params)) or (len(params[e.name]) == 0) or
+              (params[e.name].kind != vkValuesList)
+            )
+          ):
+            result &= params[e.name].values_list[i]
+
+  elif line.kind == lRef:
+    let e = line.expression
+    if (e.name in params) and (params[e.name].kind == vkParamsList):
+      for i, subparams in params[e.name].params_list:
+        if i != 0:
+          result &= '\n'
+        result &= line.bounds.left
         if not (e.optional and not (e.name in templates)):
-          let sub_params = block:
-            if e.name in params:
-              params[e.name].params_list[i]
-            else:
-              init_table[string, Value]()
-          result &= rendered(templates[e.name], sub_params, templates)
-      b = e.boundaries.b + 1
-    result &= line.source[b .. ^1] & external.right
+          result &= rendered(templates[e.name], subparams, templates,
+              line.bounds & external)
+        result &= line.bounds.right
+    else:
+      if e.optional: return ""
+      else: raise new_exception(RenderError,
+          "Parameters for non-optional subtemplate `" & e.name & "` not provided")
 
 func new_template(text: string): Template =
   for l in split_lines text:
     result.lines.add new_line l
 
-func rendered*(
+proc rendered*(
     t: Template,
     params: Params = Params(init_table[string, Value]()),
     templates: Templates = Templates(init_table[string, Template]()),
+    bounds: Bounds = ("", "")
 ): string =
   for i, l in t.lines:
     if i != 0:
       result &= '\n'
-    result &= rendered(l, params, templates)
+    result &= rendered(l, params, templates, bounds)
 
 proc test(
     t: string,
@@ -140,13 +211,15 @@ test(
 test(
   "one <!-- (param)p1 --> two <!-- (param)p2 --> three",
   "one v1 two v2 three\none v3 two v4 three",
-  {"p1": values_list @["v1", "v3"], "p2": values_list @["v2", "v4", "v5"]}.to_table,
+  {"p1": values_list @["v1", "v3"], "p2": values_list @["v2", "v4",
+      "v5"]}.to_table,
 )
 test("one <!-- (optional)(param)p1 --> two", "one  two")
 test(
   "one <!-- (ref)r --> two",
   "one three two",
-  templates = {"r": new_template("three")}.to_table,
+  {"r": params_list @[init_table[string, Value]()]}.to_table,
+  {"r": new_template("three")}.to_table,
 )
 test(
   "one <!-- (ref)r --> two",
@@ -154,9 +227,10 @@ test(
   {"r": params_list @[{"p": values_list @["three"]}.to_table]}.to_table,
   {"r": new_template "<!-- (param)p -->"}.to_table,
 )
-# test(
-#   "one <!-- (ref)r --> two",
-#   "one three two\none four two",
-#   {"r": params_list @[{"p": values_list @["three", "four"]}.to_table]}.to_table,
-#   {"r": new_template "<!-- (param)p -->"}.to_table,
-# )
+test(
+  "one <!-- (ref)r --> two",
+  "one three two\none four two",
+  {"r": params_list @[{"p": values_list @["three"]}.to_table, {
+      "p": values_list @["four"]}.to_table]}.to_table,
+  {"r": new_template "<!-- (param)p -->"}.to_table,
+)
